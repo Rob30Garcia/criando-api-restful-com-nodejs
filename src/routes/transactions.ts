03eb8432926ede33000f73b72c941c47f9ea9461
Robert Garcia <rgs2@icomp.ufam.edu.br>
2023-03-05 21:14:07 -0400

    chore: add cookie with session_id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -45,12 +45,24 @@ export async function transactionRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     // como estamos criando, não vai precisar de resposta
 
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
